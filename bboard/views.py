fb4a5296adea46b7041f76e4e1c15301c38ddc62
Anton_Work <Anton1507@bk.ru>
2023-01-10 17:01:59 +0300

    Прописал модель обьявлений добавил админку

--- bboard/views.py
+++ bboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Здесь буедт выведен список объявлений.")
 # Create your views here.
