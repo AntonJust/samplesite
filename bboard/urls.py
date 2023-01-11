1a5f83ce22e666aa2a14e66d5de7b50bb4754bd0
Anton_Work <Anton1507@bk.ru>
2023-01-11 15:17:04 +0300

    DONE TEMPLATE

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from .views import index
-
+from .views import by_rubric
+from .views import BbCreateView
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('add/', BbCreateView.as_view() , name='add'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
 from django.http import HttpResponse
+from django.template import loader
+from django.urls import reverse_lazy
+
+from .models import Bb
+from .models import Rubric
+
+from .forms import BbForm
 
 
 def index(request):
-    return HttpResponse("Здесь буедт выведен список объявлений.")
-# Create your views here.
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+
+    return HttpResponse(template.render(context, request))
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics,
+                'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
